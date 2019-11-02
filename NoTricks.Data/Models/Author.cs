657a9cf8f734a9d78c1b6a50ca32f0599f7eccc7
Tucker <bullaratucker@gmail.com>
2019-11-01 23:47:06 -0500

    changes Birthday type to DateTime

--- NoTricks.Data/Models/Author.cs
+++ NoTricks.Data/Models/Author.cs
@@ -1,9 +1,11 @@
-﻿namespace NoTricks.Data.Models {
+﻿using System;
+
+namespace NoTricks.Data.Models {
     public class Author {
         public int Id { get; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string? PenName { get; set; }
-        public string Birthday { get; set; }
+        public DateTime Birthday { get; set; }
     }
 }
