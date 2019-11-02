1a7cd4a2da0a6b79921c6efc42b205e0055d57c3
Tucker <bullaratucker@gmail.com>
2019-11-01 21:06:47 -0500

    Adds Addresses repo

---
+++ NoTricks.Data/Repositories/AddressesRepo.cs
@@ -0,0 +1,26 @@
+﻿using NoTricks.Data.Models;
+
+namespace NoTricks.Data.Repositories {
+    public class AddressesRepo : IRepository<Addresses> {
+        private readonly string _connStr;
+        
+        public AddressesRepo(NoTricksConnectionString connStr) {
+            _connStr = connStr.Value;
+        }
+        public int Insert(Addresses model) {
+            throw new System.NotImplementedException();
+        }
+
+        public Addresses GetById(int id) {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(int id) {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Update(Addresses model) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
