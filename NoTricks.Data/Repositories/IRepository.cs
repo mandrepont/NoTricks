d721dedd187f09934f35590d953dc22479d20e0c
Tucker <bullaratucker@gmail.com>
2019-11-01 22:57:44 -0500

    adds GetAll to AccountRepo

--- NoTricks.Data/Repositories/IRepository.cs
+++ NoTricks.Data/Repositories/IRepository.cs
@@ -1,7 +1,10 @@
+using System.Collections.Generic;
+
 namespace NoTricks.Data.Repositories {
     public interface IRepository<T> {
         int Insert(T model);
         T GetById(int id);
+        IEnumerable<T> GetAll();
         bool Remove(int id);
         bool Update(T model);
     }
