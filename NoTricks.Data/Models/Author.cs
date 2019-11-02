6f25f46aec34ee4a7ea121824369be72d083c53c
Tucker <bullaratucker@gmail.com>
2019-11-01 23:45:31 -0500

    makes PenName nullable

--- NoTricks.Data/Models/Author.cs
+++ NoTricks.Data/Models/Author.cs
@@ -3,7 +3,7 @@
         public int Id { get; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string PenName { get; set; }
+        public string? PenName { get; set; }
         public string Birthday { get; set; }
     }
 }
