f5d0b521caced46c46866746af0dc2a6b1d86fd3
Tucker <37936830+tbullara@users.noreply.github.com>
2019-11-01 23:11:29 -0500

    Update NoTricks.Data/Models/Addresses.cs
    Co-Authored-By: Michael <smallxmac@gmail.com>

--- NoTricks.Data/Models/Addresses.cs
+++ NoTricks.Data/Models/Addresses.cs
@@ -4,7 +4,7 @@ namespace NoTricks.Data.Models {
     public class Addresses {
         public int Id { get; }
         public string StreetAddress1 { get; set; }
-        public string StreetAddress2 { get; set; }
+        public string? StreetAddress2 { get; set; }
         public string ZipCode { get; set; }
         public string City { get; set; }
         public string State { get; set; }
