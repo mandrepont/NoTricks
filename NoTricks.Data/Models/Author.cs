06213736d9418d78d036ea920495d94ec8cd82a4
Tucker <bullaratucker@gmail.com>
2019-11-01 22:52:22 -0500

    Adds Author model

---
+++ NoTricks.Data/Models/Author.cs
@@ -0,0 +1,9 @@
+﻿namespace NoTricks.Data.Models {
+    public class Author {
+        public int Id { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PenName { get; set; }
+        public string Birthday { get; set; }
+    }
+}
