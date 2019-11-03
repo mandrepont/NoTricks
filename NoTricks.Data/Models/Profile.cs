b3b9013b402a667dac3a0a173af8054be7c0c1e1
Tucker <bullaratucker@gmail.com>
2019-11-03 10:29:45 -0600

    adds Profile model

---
+++ NoTricks.Data/Models/Profile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NoTricks.Data.Models
+{
+    public class Profile
+    {
+        public int Id { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PreferredName { get; set; }
+        public string Phone { get; set; }
+        public DateTime? Birthday { get; set; }
+        public int AddressId { get; set; }
+        public int AccountId { get; set; }
+    }
+}
