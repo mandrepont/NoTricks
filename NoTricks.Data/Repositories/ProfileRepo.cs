46528886ab126f1694e6fad23e728160c3a6ae7f
Tucker <bullaratucker@gmail.com>
2019-11-03 10:31:51 -0600

    Adds Profile Repo

---
+++ NoTricks.Data/Repositories/ProfileRepo.cs
@@ -0,0 +1,27 @@
+﻿using NoTricks.Data.Models;
+
+namespace NoTricks.Data.Repositories
+{
+    public class ProfileRepo : IRepository<Profile>
+    {
+        public int Insert(Profile model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Profile GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Update(Profile model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
