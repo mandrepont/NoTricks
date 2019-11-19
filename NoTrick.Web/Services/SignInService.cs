687051a64ab25b5c30ec1da9fd660e77a9014a47
Tucker <bullaratucker@gmail.com>
2019-11-18 21:25:54 -0600

    Adds some account validation

--- NoTrick.Web/Services/SignInService.cs
+++ NoTrick.Web/Services/SignInService.cs
@@ -1,19 +1,25 @@
 ﻿using System.Threading.Tasks;
 using NoTricks.Data.Models;
 using NoTricks.Data.Repositories;
+using Org.BouncyCastle.Security;
 
 namespace NoTrick.Web.Services {
     public class SignInService {
-        private IAccountRepo _accountRepo;
+        private readonly IAccountRepo _accountRepo;
+
+        public SignInService(IAccountRepo accountRepo) {
+            _accountRepo = accountRepo;
+        }
+        
         public AccountStatus CheckAccount(string email, string password) {
             var account = _accountRepo.GetByEmail(email);
             if (account == null) return AccountStatus.Unauthenticated;
 
-            return account.Status;
+            var hashedPassword = PasswordHasher.HashPassword(password, account.PasswordSalt);
+            
+            return hashedPassword.Item1 != account.PasswordHash ? AccountStatus.Unauthenticated : account.Status;
         }
 
-        public async Task SignIn(string email, bool rememberMe) {
-            
-        }
+        public async Task SignIn(string email, bool rememberMe) { }
     }
 }
