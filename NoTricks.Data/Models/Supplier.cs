a502917ffff1f1f82394fbc8d7785c16913795c9
Tucker <bullaratucker@gmail.com>
2019-11-29 14:23:54 -0600

    Changes balance type from double to decimal

--- NoTricks.Data/Models/Supplier.cs
+++ NoTricks.Data/Models/Supplier.cs
@@ -2,7 +2,7 @@
     public class Supplier {
         public int Id { get; }
         public string CompanyName { get; set; }
-        public double Balance { get; set; }
+        public decimal Balance { get; set; }
         public int? ManagerId { get; set; }
         public int? AddressId { get; set; }
     }
