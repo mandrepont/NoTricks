880d1621361c8ee5f6cc517fa56e4298739ee5b3
Tucker <bullaratucker@gmail.com>
2019-11-29 16:25:24 -0600

    Adds Main Supplier Page

---
+++ NoTrick.Web/Pages/Admin/Supplier/Index.cshtml.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NoTrick.Web.Pages.Admin.Supplier {
+    public class Index : PageModel {
+        public void OnGet() {
+            
+        }
+    }
+}
