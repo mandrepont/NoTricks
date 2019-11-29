eec1341fe79b961d1b79a7d2b8e5a0be9a1410cb
Tucker <bullaratucker@gmail.com>
2019-11-29 17:03:33 -0600

    Adds supplier data reading from database into front end

--- NoTrick.Web/Pages/Admin/Supplier/Index.cshtml.cs
+++ NoTrick.Web/Pages/Admin/Supplier/Index.cshtml.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NoTricks.Data.Repositories;
 
 namespace NoTrick.Web.Pages.Admin.Supplier {
     public class Index : PageModel {
+        private readonly ISupplierRepo _supplierRepo;
+        public IEnumerable<NoTricks.Data.Models.Supplier> Suppliers;
+
+        public Index(ISupplierRepo supplierRepo) {
+            _supplierRepo = supplierRepo;
+        }
+        
         public void OnGet() {
-            
+            Suppliers = _supplierRepo.GetAll();
         }
     }
 }
