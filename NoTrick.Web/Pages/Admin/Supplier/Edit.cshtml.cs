54d1890449f73caca42245012d777f50c3ea44ae
Tucker <bullaratucker@gmail.com>
2019-11-29 16:25:09 -0600

    Adds EditSupplier Page

---
+++ NoTrick.Web/Pages/Admin/Supplier/Edit.cshtml.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NoTrick.Web.Pages.Admin.Supplier {
+    public class Edit : PageModel {
+        public void OnGet() {
+            
+        }
+    }
+}
