4e88b25a4e23ddc8847aa7cd8d56ddf675915d2d
Tucker <bullaratucker@gmail.com>
2019-11-29 14:46:49 -0600

    Adds SupplierMappingRepo

---
+++ NoTricks.Data/Repositories/SupplierMappingRepo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using NoTricks.Data.Models;
+
+namespace NoTricks.Data.Repositories {
+    public interface ISupplierMappingRepo : IMappingRepository<SupplierMapping> { }
+    
+    public class SupplierMappingRepo : ISupplierMappingRepo {
+        private readonly string _connStr;
+        
+        public SupplierMappingRepo(NoTricksConnectionString connStr) {
+            _connStr = connStr.Value;
+        }
+        
+        public IEnumerable<SupplierMapping> GetAll() {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Insert(SupplierMapping model) {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Exist(SupplierMapping model) {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(SupplierMapping model) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
