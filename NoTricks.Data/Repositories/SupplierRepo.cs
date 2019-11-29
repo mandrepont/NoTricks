53c2c30e92c97595fe17a1409a89b17650bdd8e8
Tucker <bullaratucker@gmail.com>
2019-11-29 13:17:45 -0600

    Adds supplier repo

---
+++ NoTricks.Data/Repositories/SupplierRepo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using NoTricks.Data.Models;
+
+namespace NoTricks.Data.Repositories {
+    public interface ISupplierRepo : IRepository<Supplier>{}
+    
+    public class SupplierRepo : ISupplierRepo {
+        private readonly string _connStr;
+
+        public SupplierRepo(NoTricksConnectionString connStr) {
+            _connStr = connStr.Value;
+        }
+        
+        public int Insert(Supplier model) {
+            throw new System.NotImplementedException();
+        }
+
+        public Supplier GetById(int id) {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Supplier> GetAll() {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(int id) {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Update(Supplier model) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
