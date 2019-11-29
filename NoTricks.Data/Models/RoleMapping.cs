3435e98e8fa753e1126aaeb75bf23fbe0c10a70c
Tucker <bullaratucker@gmail.com>
2019-11-29 15:32:19 -0600

    Removes Id from mapping repos

--- NoTricks.Data/Models/RoleMapping.cs
+++ NoTricks.Data/Models/RoleMapping.cs
@@ -1,6 +1,5 @@
 namespace NoTricks.Data.Models {
     public class RoleMapping {
-        public int Id { get; }
         public int RoleId { get; set; }
         public int AccountId { get; set; }
     }
--- NoTricks.Data/Models/SupplierMapping.cs
+++ NoTricks.Data/Models/SupplierMapping.cs
@@ -1,6 +1,5 @@
 ﻿namespace NoTricks.Data.Models {
     public class SupplierMapping {
-        public int Id { get; }
         public int SupplierId { get; set; }
         public int AccountId { get; set; }
     }
