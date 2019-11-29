9c7926bbd692104aa4e615a6cebfa4a382b74c54
Tucker <bullaratucker@gmail.com>
2019-11-29 15:25:28 -0600

    Implements SupplierMappingRepo

--- NoTricks.Data/Repositories/SupplierMappingRepo.cs
+++ NoTricks.Data/Repositories/SupplierMappingRepo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using Dapper;
+using MySql.Data.MySqlClient;
 using NoTricks.Data.Models;
 
 namespace NoTricks.Data.Repositories {
@@ -11,20 +13,52 @@ namespace NoTricks.Data.Repositories {
             _connStr = connStr.Value;
         }
         
-        public IEnumerable<SupplierMapping> GetAll() {
-            throw new System.NotImplementedException();
-        }
-
         public bool Insert(SupplierMapping model) {
-            throw new System.NotImplementedException();
+            //Do not insert existing mapping, it throws exceptions.
+            if (Exist(model)) return false;
+            
+            using var conn = new MySqlConnection(_connStr);
+            conn.Open();
+            var sql = $@"
+                INSERT INTO SupplierMappings(SupplierId, AccountId)
+                VALUES (@{nameof(SupplierMapping.SupplierId)}, @{nameof(SupplierMapping.AccountId)});
+            ";
+            return conn.Execute(sql, model) == 1;
         }
 
         public bool Exist(SupplierMapping model) {
-            throw new System.NotImplementedException();
+            using var conn = new MySqlConnection(_connStr);
+            conn.Open();
+            var sql = $@"
+                SELECT COUNT(1) FROM SupplierMappings
+                WHERE
+                  SupplierId = @{nameof(SupplierMapping.SupplierId)} AND AccountId = @{nameof(SupplierMapping.AccountId)};
+            ";
+            return conn.ExecuteScalar<bool>(sql, model);
         }
 
         public bool Remove(SupplierMapping model) {
-            throw new System.NotImplementedException();
+            using var conn = new MySqlConnection(_connStr);
+            conn.Open();
+            var sql = $@"
+                DELETE FROM SupplierMappings
+                WHERE
+                  SupplierId = @{nameof(SupplierMapping.SupplierId)} AND AccountId = @{nameof(SupplierMapping.AccountId)};
+            ";
+            return conn.Execute(sql, model) == 1;
         }
+        
+        public IEnumerable<SupplierMapping> GetAll() {
+            using var conn = new MySqlConnection(_connStr);
+            conn.Open();
+            var sql = $@"
+               SELECT
+                 SupplierId AS {nameof(SupplierMapping.SupplierId)},
+                 AccountId AS {nameof(SupplierMapping.AccountId)}
+               FROM SupplierMappings
+            ";
+            return conn.Query<SupplierMapping>(sql);
+        }
+
     }
 }
