649eef913a57c40aa60702e17959ce37b602b0e9
Tucker <bullaratucker@gmail.com>
2019-11-29 13:53:44 -0600

    Makes ManagerId and AddressId nullable

--- NoTricks.Data/Models/Supplier.cs
+++ NoTricks.Data/Models/Supplier.cs
@@ -3,7 +3,7 @@
         public int Id { get; }
         public string CompanyName { get; set; }
         public double Balance { get; set; }
-        public int ManagerId { get; set; }
-        public int AddressId { get; set; }
+        public int? ManagerId { get; set; }
+        public int? AddressId { get; set; }
     }
 }
