71bd49dbf83cd01db54eaa0fb920ebce2218c459
Tucker <bullaratucker@gmail.com>
2019-11-29 14:37:06 -0600

    Adds SupplierMapping model

---
+++ NoTricks.Data/Models/SupplierMapping.cs
@@ -0,0 +1,7 @@
+﻿namespace NoTricks.Data.Models {
+    public class SupplierMapping {
+        public int Id { get; }
+        public int SupplierId { get; set; }
+        public int AccountId { get; set; }
+    }
+}
