ea263d46ed4eda01570aaf9b8ea80bf3c54b8b7b
Tucker <bullaratucker@gmail.com>
2019-11-30 00:01:37 -0600

    Updates SupplierManager

--- NoTricks.Data/Models/SupplierManager.cs
+++ NoTricks.Data/Models/SupplierManager.cs
@@ -1,9 +1,15 @@
-﻿namespace NoTricks.Data.Models {
+﻿using System;
+
+namespace NoTricks.Data.Models {
     public class SupplierManager {
         public int ProfileId { get; }
         public int AccountId { get; }
         public string FirstName { get; }
         public string LastName { get; }
         public string PreferredName { get; }
+
+        public string PreferredFirstLastName => String.IsNullOrWhiteSpace(PreferredName)
+            ? $"{FirstName} {LastName}"
+            : $"{PreferredName} {LastName}";
     }
 }
