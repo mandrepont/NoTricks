b21e656c6173851d30590f7ed8c2bb85aefe21d0
Tucker <bullaratucker@gmail.com>
2019-11-29 22:16:04 -0600

    Adds Manager Page and linking

---
+++ NoTrick.Web/Pages/Admin/Supplier/Managers.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using NoTrick.Web.Pages.Supplier;
+using NoTricks.Data.Repositories;
+
+namespace NoTrick.Web.Pages.Admin.Supplier {
+    public class Managers : PageModel {
+        public NoTricks.Data.Models.Supplier supplier { get; set; }
+        private readonly ISupplierRepo _supplierRepo;
+        public Managers(ISupplierRepo supplierRepo) {
+            _supplierRepo = supplierRepo;
+        }
+        
+        public void OnGet(int id) {
+            supplier = _supplierRepo.GetById(id);
+        }
+    }
+}
