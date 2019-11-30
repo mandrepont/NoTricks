48bae78c22fbb7503f6ae6a2ef793fc75c3aca21
Tucker <bullaratucker@gmail.com>
2019-11-29 22:45:57 -0600

    Adds SupplierManager model

---
+++ NoTricks.Data/Models/SupplierManager.cs
@@ -0,0 +1,9 @@
+﻿namespace NoTricks.Data.Models {
+    public class SupplierManager {
+        public int ProfileId { get; }
+        public int AccountId { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string PreferredName { get; }
+    }
+}
