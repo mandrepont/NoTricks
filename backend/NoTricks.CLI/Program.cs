9858b6e66d957326daddb30b2551572fc217f824
Tucker <bullaratucker@gmail.com>
2020-11-18 19:01:00 -0600

    Adds NoTricks.Data and NoTricks.CLI

---
+++ backend/NoTricks.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NoTricks.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ backend/NoTricks.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NoTricks.Data
+{
+    public class Class1
+    {
+    }
+}
